93396af1afd0ff8bdc1867a18fb671e41663ae55
Pedro Neto <pedroseverino@msn.com>
2020-03-29 16:16:40 -0300

    Correção no Script de Banco

--- scriptsBd/DDL/initialCreate.sql
+++ scriptsBd/DDL/initialCreate.sql
@@ -1,28 +1,28 @@
 create table employees
-(id varchar(32) not null primary key,
+(id varchar(32) not null primary key default replace(uuid(), '-', ''),
 name varchar(100) not null,
-email varchar(100) not null,
+email varchar(100) not null unique,
 maxCompanion int not null default 1);
 
 
 create table companions
-(id varchar(32) not null primary key,
-name varchar(100) not null);
+(id varchar(32) not null primary key default replace(uuid(), '-', ''),
+name varchar(100) not null unique);
 
 create table users
-(id varchar(32) not null primary key,
-email varchar(100) not null,
+(id varchar(32) not null primary key default replace(uuid(), '-', ''),
+email varchar(100) not null unique,
 password varchar(100) not null);
 
 create table events
-(id varchar(32) not null primary key,
+(id varchar(32) not null primary key default replace(uuid(), '-', ''),
 maxCompanion int not null default 0,
-name varchar(100) not null,
+name varchar(100) not null unique,
 description varchar(100),
 place varchar(100));
 
 create table eventsEmployees
-(id varchar(32) not null primary key,
+(id varchar(32) not null primary key default replace(uuid(), '-', ''),
 idEvent varchar(32) not null,
 idEmployee varchar(32) not null);
 
@@ -30,9 +30,10 @@ alter table eventsEmployees add foreign key (idEvent) references events(id);
 alter table eventsEmployees add foreign key (idEmployee) references employees(id);
 
 create table eventsEmployeesCompanions
-(idEventEmployee varchar(32) not null,
-idCompanion varchar(32) not null,
-primary key (idEventEmployee, idCompanion));
+(id varchar(32) not null primary key default replace(uuid(), '-', ''),
+idEventEmployee varchar(32) not null,
+idCompanion varchar(32) not null);
 
 alter table eventsEmployeesCompanions add foreign key (idEventEmployee) references eventsEmployees(id);
 alter table eventsEmployeesCompanions add foreign key (idCompanion) references companions(id);
+alter table eventsemployeescompanions add unique(idEventEmployee, idCompanion);
